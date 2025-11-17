399794e3a1eb867b85ef542e80e3ff1aa8637784
FAN <465989260@qq.com>
2025-11-17 16:28:11 +0800

    完成ui界面和游戏状态

--- assets/code/GameManager.ts
+++ assets/code/GameManager.ts
@@ -1,10 +1,16 @@
-import { _decorator, Component, Prefab, CCInteger, instantiate, Node } from 'cc';
+import { _decorator, Component, Prefab, CCInteger, instantiate, Node,Vec3,Label } from 'cc';
 import { BLOCK_SIZE, PlayerController } from './PlayerController';
 const { ccclass, property } = _decorator;
-
+//block块
 enum BlockType{
-    BT_NONE,
-    BT_STONE,
+    BT_NONE,//表示坑
+    BT_STONE,//表示方块
+};
+//枚举描述游戏状态
+enum GameState{
+    GS_INIT,
+    GS_PLAYING,
+    GS_END,
 };
 
 @ccclass('GameManger')
@@ -12,12 +18,124 @@ export class GameManger extends Component {
 
 
     @property({type: Prefab})
-    public boxPrefab: Prefab|null = null;
+    public boxPrefab: Prefab|null = null;//指向已经创建好的Box预制体
+
+    @property({type: CCInteger})
+    public roadLength: number = 50;
+    private _road: BlockType[] = [];//初始化道路长度
+    //GS_INIT游戏初始化状态
+    @property({ type: Node })
+    public startMenu: Node | null = null; // 开始的 UI
+    @property({ type: PlayerController }) 
+    public playerCtrl: PlayerController | null = null; // 角色控制器
+    @property({type: Label}) 
+    public stepsLabel: Label|null = null; // 计步器
 
     start() {
-
+        this.setCurState(GameState.GS_INIT); // 第一初始化要在 start 里面调用
+        this.playerCtrl?.node.on('JumpEnd', this.onPlayerJumpEnd, this);
     }
+    //生成地图
+    generateRoad() {
 
+        this.node.removeAllChildren();
+
+        this._road = [];
+        // startPos
+        this._road.push(BlockType.BT_STONE);
+
+        for (let i = 1; i < this.roadLength; i++) {
+            if (this._road[i - 1] === BlockType.BT_NONE) {
+                this._road.push(BlockType.BT_STONE);
+            } else {
+                this._road.push(Math.floor(Math.random() * 2));
+            }
+        }
+        
+        for (let j = 0; j < this._road.length; j++) {
+            let block: Node | null = this.spawnBlockByType(this._road[j]);
+            if (block) {
+                this.node.addChild(block);
+                block.setPosition(j * BLOCK_SIZE, 0, 0);
+            }
+        }
+    }
+    //spawn创建新进程
+    spawnBlockByType(type: BlockType) {
+        if (!this.boxPrefab) {
+            return null;
+        }
+
+        let block: Node|null = null;
+        switch(type) {
+            case BlockType.BT_STONE:
+                block = instantiate(this.boxPrefab);
+                break;
+        }
+
+        return block;
+    }
+    //控制游戏状态
+    setCurState (value: GameState) {
+        switch(value) {
+            case GameState.GS_INIT:            
+                this.init();
+                break;
+            case GameState.GS_PLAYING: 
+                if (this.startMenu) {
+                    this.startMenu.active = false;
+                }
+
+                if (this.stepsLabel) {
+                    this.stepsLabel.string = '0';   // 将步数重置为0
+                }
+
+                setTimeout(() => {      //直接设置active会直接开始监听鼠标事件，做了一下延迟处理
+                    if (this.playerCtrl) {
+                        this.playerCtrl.setInputActive(true);
+                    }
+                }, 0.1);          
+                break;
+            case GameState.GS_END:
+                break;
+        }
+    }
+    //进入到GS_INIT时的处理
+    init() {       
+        if (this.startMenu) {
+            this.startMenu.active = true;
+        }
+
+        this.generateRoad();
+
+        if (this.playerCtrl) {
+            this.playerCtrl.setInputActive(false);
+            this.playerCtrl.node.setPosition(Vec3.ZERO);
+            this.playerCtrl.reset();
+        }
+    }
+    //响应play按钮按下的事件
+    onStartButtonClicked() {    
+        this.setCurState(GameState.GS_PLAYING);
+    }
+    //监听角色跳跃结束的事件
+    onPlayerJumpEnd(moveIndex: number) {
+        if (this.stepsLabel) {
+            this.stepsLabel.string = '' + (moveIndex >= this.roadLength ? this.roadLength : moveIndex);
+        }
+    this.checkResult(moveIndex);
+    }
+    //判定角色是否跳跃到坑或者跳完所有地块
+    checkResult(moveIndex: number) {
+        if (moveIndex < this.roadLength) {
+            if (this._road[moveIndex] == BlockType.BT_NONE) {   //跳到了空方块上
+                
+                this.setCurState(GameState.GS_INIT)
+            }
+        } else {    // 跳过了最大长度            
+            this.setCurState(GameState.GS_INIT);
+        }
+    }
     update(deltaTime: number) {
         
     }
--- assets/code/PlayerController.ts
+++ assets/code/PlayerController.ts
@@ -19,13 +19,28 @@ export class PlayerController extends Component {
     private _deltaPos: Vec3 = new Vec3(0, 0, 0);
     private _targetPos: Vec3 = new Vec3();
 
+    //记录角色当前为第几步
+    private _curMoveIndex: number = 0;
+
     start () {
-        input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        //监听输入
+        // input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
     }
-
+    //动态开启和关闭角色对鼠标消息的监听
+    setInputActive(active: boolean) {
+        if (active) {
+            input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        } else {
+            input.off(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        }
+    }
+    //重置步数属性
     reset() {
-    }   
-
+        this._curMoveIndex = 0;
+        this.node.getPosition(this._curPos);
+        this._targetPos.set(0,0,0);
+    }  
+    //监听鼠标输入
     onMouseUp(event: EventMouse) {
         if (event.getButton() === 0) {
             this.jumpByStep(1);
@@ -34,7 +49,7 @@ export class PlayerController extends Component {
         }
 
     }
-
+    //根据步数跳跃
     jumpByStep(step: number) {
         if (this._startJump) {
             return;
@@ -58,15 +73,21 @@ export class PlayerController extends Component {
                 this.BodyAnim.play('twoStep');
             }
         }
+        this._curMoveIndex += step;
     }
-   
+    //监听跳跃结束
+    onOnceJumpEnd() {
+        this.node.emit('JumpEnd', this._curMoveIndex);
+    }
+    //根据每次更新来计算角色的最新位置
     update (deltaTime: number) {
         if (this._startJump) {
             this._curJumpTime += deltaTime;
             if (this._curJumpTime > this._jumpTime) {
                 // end
                 this.node.setPosition(this._targetPos);
-                this._startJump = false;              
+                this._startJump = false;
+                this.onOnceJumpEnd();               
             } else {
                 // tween
                 this.node.getPosition(this._curPos);
